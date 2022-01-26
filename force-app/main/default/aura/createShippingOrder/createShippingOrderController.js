bd652e4fc12f6a189228c51bf0872541d9384962
Paulina Szyszko <paulinabuczel@gmail.com>
2022-01-26 17:24:19 +0100

    added closing Quick Action after submit

--- force-app/main/default/aura/createShippingOrder/createShippingOrderController.js
+++ force-app/main/default/aura/createShippingOrder/createShippingOrderController.js
@@ -2,8 +2,10 @@
     myAction : function(component, event, helper) {
 
     },
-
+    handleClick : function (cmp, event, helper) {
+        $A.get("e.force:closeQuickAction").fire();
+        $A.get("e.force:refreshView").fire();
+    }
    
-
  
 })
