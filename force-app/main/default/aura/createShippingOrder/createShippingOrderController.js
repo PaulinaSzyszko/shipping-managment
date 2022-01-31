67c05f9d5335f22e2d2585253a0ae33980e60755
Paulina Szyszko <paulinabuczel@gmail.com>
2022-01-31 09:23:15 +0100

    create task trigger

--- force-app/main/default/aura/createShippingOrder/createShippingOrderController.js
+++ force-app/main/default/aura/createShippingOrder/createShippingOrderController.js
@@ -1,7 +1,5 @@
 ({
-    myAction : function(component, event, helper) {
-
-    },
+    
     handleClick : function (cmp, event, helper) {
         $A.get("e.force:closeQuickAction").fire();
         $A.get("e.force:refreshView").fire();
